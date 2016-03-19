5ad219eaf44df059276cbd74b1b0cb009ea2d4ba
Lukas Boehler <lukas.boehler@me.com>
2016-03-19 17:13:31 +0100

    FANCY!

--- awesome/TVButler/SetupInterestsViewController.m
+++ awesome/TVButler/SetupInterestsViewController.m
@@ -9,6 +9,8 @@
 #import "SetupInterestsViewController.h"
 #import "YSLDraggableCardContainer.h"
 #import "CardView.h"
+#import <AFNetworking/AFHTTPSessionManager.h>
+#import <AFNetworking/UIImageView+AFNetworking.h>
 
 #define RGB(r, g, b)     [UIColor colorWithRed: (r) / 255.0 green: (g) / 255.0 blue: (b) / 255.0 alpha : 1]
 
@@ -29,21 +31,39 @@
     [super viewDidLoad];
     
     _container = [[YSLDraggableCardContainer alloc]init];
-    _container.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    double topSpacing = 80.0;
+    _container.frame = CGRectMake(0, topSpacing, self.view.frame.size.width, self.view.frame.size.height - topSpacing);
     _container.backgroundColor = [UIColor clearColor];
     _container.dataSource = self;
     _container.delegate = self;
     _container.canDraggableDirection = YSLDraggableDirectionLeft | YSLDraggableDirectionRight;
     [self.view addSubview:_container];
     
-    _datas = [NSMutableArray array];
-    for (int i = 0; i < 7; i++) {
-        NSDictionary *dict = @{@"image" : [NSString stringWithFormat:@"photo_sample_0%d",i + 1],
-                               @"name" : @"YSLDraggableCardContainer Demo"};
-        [_datas addObject:dict];
-    }
+    NSString * sevenHackApiURLRequest = @"http://api.7hack.de:80/v1/tvshows?apikey=9VeEdUMw&selection=%7BtotalCount%2Cdata%7Bid%2Ctitles%7Bdefault%7D%2Ckeyworkds%7Bdefault%7D%2Cgenres%2Cimages%28subType%3A%22Cover%20Big%22%29%7Burl%2CsubType%7D%7D%7D";
     
-    [_container reloadCardContainer];
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET: sevenHackApiURLRequest parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+        
+        NSArray * tvShow = [[responseObject objectForKey: @"response"] objectForKey: @"data"];
+        _datas = [NSMutableArray array];
+        for (int i = 0; i < tvShow.count; i++) {
+            NSDictionary * currentTVShow = [tvShow objectAtIndex: i];
+            
+            @try {
+                NSDictionary *dict = @{@"image" : [[[currentTVShow objectForKey: @"images"] objectAtIndex: 0] objectForKey: @"url"],
+                                       @"name" : [[currentTVShow objectForKey: @"titles"] objectForKey: @"default"]};
+                [_datas addObject:dict];
+            }
+            @catch (NSException *exception) {
+            }
+            @finally {
+            }
+        }
+        [_container reloadCardContainer];
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 #pragma mark -- YSLDraggableCardContainer DataSource
@@ -52,7 +72,8 @@
     NSDictionary *dict = _datas[index];
     CardView *view = [[CardView alloc]initWithFrame:CGRectMake(10, 64, self.view.frame.size.width - 20, self.view.frame.size.width - 20)];
     view.backgroundColor = [UIColor whiteColor];
-    view.imageView.image = [UIImage imageNamed:dict[@"image"]];
+    [view.imageView setImageWithURL: [NSURL URLWithString: dict[@"image"]]];
+    view.imageView.contentMode = UIViewContentModeScaleAspectFill;
     view.label.text = [NSString stringWithFormat:@"%@  %ld",dict[@"name"],(long)index];
     return view;
 }
