9fafbccbeb53a9e9feb5c16081a746aeba09a7eb
Lukas Boehler <lukas.boehler@me.com>
2016-03-19 16:11:58 +0100

    some delegates
    some delegates

--- awesome/TVButler/ViewController.m
+++ awesome/TVButler/ViewController.m
@@ -25,12 +25,20 @@
     if(![PFUser currentUser]) {
         PFLogInViewController *logInViewController = [[PFLogInViewController alloc] init];
         logInViewController.signUpController.emailAsUsername = YES;
+        logInViewController.signUpController.delegate = self;
         logInViewController.emailAsUsername = YES;
         logInViewController.delegate = self;
         [self presentViewController:logInViewController animated: NO completion:nil];
     } else {
         // We are logged in. Go party!
     }
+}
+
+- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
+    // log the user in.
+    [signUpController dismissViewControllerAnimated: YES completion:^{
+        
+    }];
 }
 
 - (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
