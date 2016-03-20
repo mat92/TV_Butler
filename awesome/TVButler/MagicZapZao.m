830f8dc8d24744d0d0918e1a038ca471ed97cb40
Lukas Boehler <lukas.boehler@me.com>
2016-03-20 03:46:14 +0100

    aaaa
    aaaa

--- awesome/TVButler/MagicZapZao.m
+++ awesome/TVButler/MagicZapZao.m
@@ -28,7 +28,7 @@
      http://hack.api.uat.ebmsctogroup.com/stores-active/contentInstance/event/filter?numberOfResults=100&filter={"criteria":[{"term":"publishedStartDateTime","operator":"atLeast","value":"2016-03-19T18:00:00Z"},{"term":"publishedStartDateTime","operator":"atMost","value":"2016-03-19T22:00:00Z"},{"term":"sourceName","operator":"in","values":["RTL","ProSieben"]}],"operator":"and"}&api_key=240e4458fc4c6ac85c290481646b21ef
      */
     
-    NSString * sevenHackApiURLRequest = @"";
+    /*NSString * sevenHackApiURLRequest = @"";
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     [manager GET: sevenHackApiURLRequest parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
         NSLog(@"JSON: %@", responseObject);
@@ -37,7 +37,9 @@
         
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         NSLog(@"Error: %@", error);
-    }];
+    }];*/
+    
+    finishBlock(@"PRO7");
 }
 
 @end
